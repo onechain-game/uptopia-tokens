d3f3ebe0972f896369b760f945d2c3942622ec72
Bao Tran <bao.tran080898@gmail.com>
2026-06-23 09:14:33 +0700

    use erc20 permit and burnable

--- contracts/Up.sol
+++ contracts/Up.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 /// @title Up
 /// @notice ERC-20 token with a fixed supply of 500,000,000 UP.
-contract Up is ERC20 {
+contract Up is ERC20, ERC20Permit, ERC20Burnable {
     uint256 public constant TOTAL_SUPPLY = 500_000_000 * 1e18;
 
     error ZeroReceiver();
